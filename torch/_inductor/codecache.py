da0efd3e6ca35ac3126493667ef129c0d1ba2873
Guilherme Leobas <guilhermeleobas@gmail.com>
2023-10-04 13:02:53 -0300

    Move negative index checking to common.py and raise exception on out-of-bounds tensor indexing
    ghstack-source-id: fbf499b673d84541d79ad01758baac07376a5f45
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/108690

--- torch/_inductor/codecache.py
+++ torch/_inductor/codecache.py
@@ -797,10 +797,13 @@ def get_include_and_linking_paths(
         lpaths = cpp_extension.library_paths(cuda) + [
             sysconfig.get_config_var("LIBDIR")
         ]
-        libs = []
+
+        # Unconditionally link with "c10" to use TORCH_CHECK. See PyTorch#108690
+        libs = ["c10"]
+
         # No need to manually specify libraries in fbcode.
         if not config.is_fbcode():
-            libs += ["c10", "torch", "torch_cpu"]
+            libs += ["torch", "torch_cpu"]
             libs += ["gomp"]
             if not aot_mode:
                 libs += ["torch_python"]
